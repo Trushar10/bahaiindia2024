9fe8476c90ff7c03558c5ff3c243d05d21dd77e1
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-05-17 19:37:12 +0530

    img height increased a bit

--- header.php
+++ header.php
@@ -15,6 +15,18 @@ if ( ! defined( 'ABSPATH' ) ) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
     <?php wp_head(); ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6NH4R7619B"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-6NH4R7619B');
+    </script>
 </head>
 
 <body>
